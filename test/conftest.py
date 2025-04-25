5f4e8c33cc009c4610420b1a85026522a650d643
Alisia Fadini <af840@cam.ac.uk>
2025-04-25 14:15:00 -0700

    keep adding

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,9 @@ from unittest.mock import MagicMock
 # Mock the package and all submodules you import from
 sys.modules["openfold"] = MagicMock()
 sys.modules["openfold.model"] = MagicMock()
+sys.modules["openfold.utils"] = MagicMock()
+sys.modules["openfold.config"] = MagicMock()
+sys.modules["openfold.data"] = MagicMock()
 sys.modules["openfold.model.model"] = MagicMock()
 sys.modules["reciprocalspaceship"] = MagicMock()
 sys.modules["SFC_Torch"] = MagicMock()
